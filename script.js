688a1f759d88ca881af392c4b67387aebe2b564d
maryamasif2008 <marayam.asif123@gmail.com>
2025-01-11 21:22:04 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+                                       //CHAPTER # 01
+                                      
+                                      /* Question 01 */
+alert("Welcome to our website");               
+
+                                      /* Question 02 */
+alert("Sorry! Please enter a valid password.")
+  
+                                      /* Question 03 */
+alert("Welcome to JS Land... \n Hppy Codding!");
+
+                                     /* Questio 04 */
+alert("Welcome to JS Land...");
+alert("Happy Codding! \n Prevnt this page from creating additional dialogs.")     
+var message ="Hello... I can run JS through my web browser's console";
+console.log(message)                                        
+                                     
+                                     /* Question 05 */
+console.log("Your desired message goes here!");
+                                     
+                                     /* Question 06 */
+document.getElementById('alertButton').addEventListener('click', function() {
+    alert('Hello, welcome to my website!');
+  });
+
+                                     /* Question 07 */
+// same as question 06                                     
+                                       
+                                     
